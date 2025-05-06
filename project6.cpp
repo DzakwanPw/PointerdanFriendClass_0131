b5301b809b5549ba07dca0dc94fdcc856587f0c8
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-06 19:36:58 +0700

    commit12

--- project6.cpp
+++ project6.cpp
@@ -2,3 +2,45 @@
 #include <string>
 using namespace std;
 
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a);
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(int pId)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("joko Susilo");
+    siswa_joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko)
+    return 0;
+}
