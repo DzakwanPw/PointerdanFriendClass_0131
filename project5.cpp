0ac49f8dc1fe3ab27b10d424fa46dbb6316e7107
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-06 19:35:52 +0700

    commit11

--- project5.cpp
+++ project5.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 using namespace std;
 
+class pelajar;
+class manusia
+{
+    public:
+    void showNimPelajar(pelajar&a);
+};
+
+class pelajar
+{
+private:
+int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &a);
+};
+
+void manusia::showNilaiPelajar(pelajar &a)
+{
+    cout << x.nilai;
+}
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
